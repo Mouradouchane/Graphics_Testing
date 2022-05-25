8e567a27c22efef79ef0454a18ed7313bc08d897
Mouradouchane <mouradouchane@gmail.com>
2022-05-25 16:29:06 +0000

    static function render in MESH class for rendering MESHES

--- canvas.js
+++ canvas.js
@@ -19,82 +19,8 @@ const ndc = new NDC(-1 , 1 , -1 , 1 , 1 , -1 , canvas.height / canvas.width , 90
 // =============== objects and meshes ===============
 let pshape = new mesh();
 
-function render_coordinates(CTX = ctx , color = "white", point , render_info = false , render_points = false){
 
-    
-    let x = ( point.x ) * canvas.width;
-    let y = ( point.y ) * canvas.height;
-    
-    if(render_info){
-        let ndc_x = point.x;
-        let ndc_y = point.y;
-
-        CTX.font = 'bold 12px serif';
-        CTX.fillStyle = color;
-        CTX.fillText(`x=${ndc_x}`,x+5,y);
-        CTX.fillText(`y=${ndc_y}`,x+5,y+20);
-        CTX.fillText(`z=${point.z}`,x+5,y+40);
-        CTX.fillText(`w=${point.w}`,x+5,y+60);
-    }
-
-    if(render_points){
-        CTX.fillStyle = "white";
-        CTX.beginPath();
-        CTX.arc(x,y,4,0,Math.PI*2);
-        CTX.fill(); 
-    }
-    
-}
-
-function render_mesh( CTX = ctx , MESH , colors = true , just_line = false , debug = false){
-    //debugger
-
-    for(let trig of MESH.triangles){
-        if(trig != null){
-            let a = trig.a;
-            let b = trig.b;
-            let c = trig.c;
-
-            if(!just_line){
-
-                CTX.fillStyle = (colors) ? trig.color : "white";
-                CTX.beginPath();
-                CTX.moveTo((a.x ) * canvas.width , (a.y ) * canvas.height);
-                CTX.lineTo((b.x ) * canvas.width , (b.y ) * canvas.height);
-                CTX.lineTo((c.x ) * canvas.width , (c.y ) * canvas.height);
-                CTX.lineTo((a.x ) * canvas.width , (a.y ) * canvas.height);
-                CTX.fill();
-
-            }
-            else{
-
-                ctx.lineWidth   = 1;
-                CTX.strokeStyle = (colors) ? trig.color : "white";
-
-                CTX.beginPath();
-                CTX.moveTo((a.x ) * canvas.width , (a.y ) * canvas.height);
-                CTX.lineTo((b.x ) * canvas.width , (b.y ) * canvas.height);
-                CTX.lineTo((c.x ) * canvas.width , (c.y ) * canvas.height);
-                CTX.lineTo((a.x ) * canvas.width , (a.y ) * canvas.height);
-
-                CTX.stroke();
-
-                render_coordinates(CTX,"red",a,false,true);
-                render_coordinates(CTX,"yellow",b,false,true);
-                render_coordinates(CTX,"cyan",c,false,true);
-            }
-            if(debug){
-                render_coordinates(CTX,"red",a,true,true);
-                render_coordinates(CTX,"yellow",b,true,true);
-                render_coordinates(CTX,"cyan",c,true,true);
-            }
-        }
-
-    }
-}
-
-move(shape);
-
+// move all MESHES and do projection 
 document.addEventListener("keydown" , (e) => {
     
     pshape = move( e , shape )[0];
@@ -110,13 +36,14 @@ function render(){
 
     setTimeout(() =>{
         //debugger
+        
         // =============== clear ===============
         ctx.clearRect(0,0,canvas.width,canvas.height);
         ctx.fillStyle = "black";
         ctx.fillRect(0,0,canvas.width,canvas.height);
 
         // =============== triangle ============  
-        render_mesh(ctx , pshape , false , true , true );
+        mesh.render(ctx , pshape , false , true , true);
 
         // =============== FPS =================
         if(fps.draw){
--- mesh.js
+++ mesh.js
@@ -58,5 +58,88 @@ export class mesh{
                 else return false;
             })
         }
+
+
     }
+
+
+    static render( CTX , MESH , colors = true , lines = false , points = false , info = false){
+        
+        //debugger
+
+        // loop over tirangles
+        for(let trig of MESH.triangles){
+
+            if(trig != null){
+            
+                let a = trig.a;
+                let b = trig.b;
+                let c = trig.c;
+
+                CTX.lineWidth   = 1;
+                CTX.fillStyle   = (colors) ? trig.color : "white";
+                CTX.strokeStyle = "white";
+
+                CTX.beginPath();
+                CTX.moveTo((a.x ) * canvas.width , (a.y ) * canvas.height);
+                CTX.lineTo((b.x ) * canvas.width , (b.y ) * canvas.height);
+                CTX.lineTo((c.x ) * canvas.width , (c.y ) * canvas.height);
+                CTX.lineTo((a.x ) * canvas.width , (a.y ) * canvas.height);
+                
+                if(lines){
+
+                    CTX.stroke();
+
+                }
+
+                if(colors){
+
+                    CTX.fill();
+
+                }
+
+                
+                if(points){
+                    mesh.render_coordinates(CTX,"red",a,true,true);
+                    mesh.render_coordinates(CTX,"yellow",b,true,true);
+                    mesh.render_coordinates(CTX,"cyan",c,true,true);
+                }
+                else{
+                    mesh.render_coordinates(CTX,"red",a,true,false);
+                    mesh.render_coordinates(CTX,"yellow",b,true,false);
+                    mesh.render_coordinates(CTX,"cyan",c,true,false); 
+                }
+                
+            }
+
+        }
+          
+    }
+    
+    static render_coordinates(CTX , color = "pink", point , info = false , points = false){
+
+        let x = ( point.x ) * canvas.width;
+        let y = ( point.y ) * canvas.height;
+        
+        if(info){
+            let ndc_x = point.x;
+            let ndc_y = point.y;
+    
+            CTX.font = 'bold 12px serif';
+            CTX.fillStyle = color;
+            
+            CTX.fillText(`x=${ndc_x}`,x+5,y);
+            CTX.fillText(`y=${ndc_y}`,x+5,y+20);
+            CTX.fillText(`z=${point.z}`,x+5,y+40);
+            CTX.fillText(`w=${point.w}`,x+5,y+60);
+        }
+    
+        if(points){
+            CTX.fillStyle = "yellow" ;
+            CTX.beginPath();
+            CTX.arc(x,y,4,0,Math.PI*2);
+            CTX.fill(); 
+        }
+    }
+       
 }
