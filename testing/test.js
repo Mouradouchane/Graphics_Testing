52aeb749f3fa2d0855898976b6f177aca4c16b9a
Mouradouchane <mouradouchane@gmail.com>
2022-06-02 07:53:40 +0000

    xy clipping in testing side top-left top-right

--- testing/test.js
+++ testing/test.js
@@ -7,8 +7,8 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
 let ln = new line(
-    new point(-810,0,0,0),
-    new point(200,350,0,0),
+    new point(10,-100,0,0),
+    new point(-200,10,0,0),
 );
 
 function check_xy(p = new point() , xmin = 0 , xmax = canvas.clientWidth , ymin = 0 , ymax = canvas.clientHeight){
@@ -26,9 +26,8 @@ function check_xy(p = new point() , xmin = 0 , xmax = canvas.clientWidth , ymin 
 
 function calc_x_intercept( p1 , p2 , xintercept = 0 ){
 
-    //   slope = (y1 - y0) / (x1 - x0)
-    //   x = x0 + (1 / slope) * (ym - y0), where ym is ymin or ymax
-    //   y = y0 + slope * (xm - x0), where xm is xmin or xmax
+    // slope = (y1 - y0) / (x1 - x0)
+    // y = y0 + slope * (xm - x0), where xm is xmin or xmax
     
     let slope = (p2.y - p1.y) / (p2.x - p1.x);
 
@@ -41,16 +40,34 @@ function calc_x_intercept( p1 , p2 , xintercept = 0 ){
     return new line(p1 , p2);
 }
 
+function calc_y_intercept( p1 , p2 , yintercept = 0 ){
+
+    //   slope = (y1 - y0) / (x1 - x0)
+    //   x = x0 + (1 / slope) * (ym - y0), where ym is ymin or ymax
+    
+    let slope = (p2.y - p1.y) / (p2.x - p1.x);
+
+    let x = p1.x + (1 / slope) * (yintercept - p1.y);
+    let y = yintercept;
+
+    p2.x = x;
+    p2.y = y;
+
+    return new line(p1 , p2);
+}
+
 function line_clipping_xy( p1 , p2 ){
     
     debugger
     
+    let ln = new line(p1,p2);
+
     let c1 = check_xy(p1);
     let c2 = check_xy(p2);
 
     if(c1 == 0 && c2 == 0 ){
         console.log( "2 inside ; 0 outside");
-        return new line(p1 , p2); // all inside
+        return ln; // all inside
     } 
 
     if(c1 != 0 && c1 == c2){
@@ -59,37 +76,54 @@ function line_clipping_xy( p1 , p2 ){
     } 
     else{
 
-        // p1 inside & p2 outside
-        if(c1 == 0 && c2 != 0){
-
-            // left clipping
-            if(c2 == 1){ 
-                return calc_x_intercept(p1 , p2 , 0);
-            }
-
-            // right clipping 
-            if(c2 == 2) {      
-                return calc_x_intercept(p1 , p2 , canvas.clientWidth);
-            }
-
-        } 
-
-
-        // p2 inside & p1 outside
+        // swap if p2 inside and p1 outside
         if(c1 != 0 && c2 == 0){
 
-            // left clipping
-            if(c1 == 1){ 
-                return calc_x_intercept(p2 , p1 , 0);
-            }
-
-            // right clipping 
-            if(c1 == 2) {
-                return calc_x_intercept(p2 , p1 , canvas.clientWidth);
-            }
-
+            //[p1,p2] = [p2,p1];
+            ln = new line( p2 , p1 );
+            [c1,c2] = [c2,c1];
         }
 
+        // left clipping
+        if(c2 == 1){ 
+            ln = calc_x_intercept(ln.p1 , ln.p2 , 0);
+        }
+
+        // right clipping 
+        if(c2 == 2) {      
+            ln = calc_x_intercept(ln.p1 , ln.p2 , canvas.clientWidth);
+        }
+
+        // top clipping
+        if(c2 == 8){
+            ln = calc_y_intercept(ln.p1 , ln.p2 , 0);
+        }
+
+        // bottom clipping
+        if(c2 == 4){
+            ln = calc_y_intercept(ln.p1 , ln.p2 , canvas.clientHeight);
+        }
+
+        // top & left clipping
+        if(c1 == 8 && c2 == 1 || c1 == 1 &&  c2 == 8){
+            ln = calc_x_intercept(ln.p1 , ln.p2 , 0);
+            ln = calc_y_intercept(ln.p2 , ln.p1 , 0);
+        }
+
+        // top & left clipping
+        if(c1 == 8 && c2 == 2 || c1 == 2 &&  c2 == 8){
+            ln = calc_x_intercept(ln.p1 , ln.p2 , canvas.clientWidth);
+            ln = calc_y_intercept(ln.p2 , ln.p1 , 0);
+        }
+
+
+        if(ln.p1.x < 0 && ln.p1.y == 0) ln.p1 = null;
+        if(ln.p2.x < 0 && ln.p2.y == 0) ln.p2 = null;
+        
+        if(ln.p1.x < 0 && ln.p1.y == 0) ln.p1 = null;
+        if(ln.p2.x < 0 && ln.p2.y == 0) ln.p2 = null;
+
+        return ln;
     }
 
 }
